a822d34a7bd2a41c01acca788039603675b36bdd
Juan David Zapata Cruz <juanda.zapatac@gmail.com>
2023-08-05 17:49:01 -0500

    Comienzo del proyecto asistente de contabilidad, input creado

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-#Codigo para el cálculo de la utilidad de una empresa a partir de sus registros mensuales de contabilidad
+#Asistente de contabilidad de tu empresa
 
 from functools import reduce
 from data_importing import import_data
 
 def run():
+    respuesta=input('''Bienvenid@ a tu asistente de contabilidad,          
+¿Qué deseas conocer hoy?
+1. Utilidades
+2. Ventas
+3. Gastos
+:''')
     data=import_data()
-    #Version reduce
-    ventas=[row[1] for row in data]
-    gastos=[row[2] for row in data]
-    print('La utilidad total es',str(reduce(lambda x,y:int(x)+int(y),ventas)-reduce(lambda x,y:int(x)+int(y),gastos)),'COP')
+    ventas=[int(row[1]) for row in data]
+    gastos=[int(row[2]) for row in data]
+    if respuesta=='1':
+        print('La utilidad total es',str(reduce(lambda x,y:x+y,ventas)-reduce(lambda x,y:x+y,gastos)),'COP')
+    elif respuesta=='2':
+        print('Las ventas totales son de',str(reduce(lambda x,y:x+y,ventas)),'COP')
+    elif respuesta=='3':
+        print('Los gastos totales son de',str(reduce(lambda x,y:x+y,gastos)),'COP')
+    else:
+        print('Error en el valor ingresado')
         
 if __name__=='__main__':
     run()
