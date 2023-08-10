95a53a76bba8e16a1f052e77b5f7dc970be3dee2
Juan David Zapata Cruz <judzapatacr@unal.edu.co>
2023-08-10 16:45:28 -0500

    Cuarta opción agregada

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def run():
 1. Utilidades
 2. Ventas
 3. Gastos
+4. Ventas promedio por mes
 :''')
     data=import_data()
     ventas=[int(row[1]) for row in data]
@@ -19,6 +20,8 @@ def run():
         print('Las ventas totales son de',str(reduce(lambda x,y:x+y,ventas)),'COP')
     elif respuesta=='3':
         print('Los gastos totales son de',str(reduce(lambda x,y:x+y,gastos)),'COP')
+    elif respuesta=='4':
+        print('Las ventas promedio por mes son de',str(reduce(lambda x,y:x+y,ventas)/len(ventas)),'COP')
     else:
         print('Error en el valor ingresado')
         
